8d815f6d609dae4dca721bb6ae64cbd13d78190e
Amitay Dobo <amitayd@gmail.com>
2014-04-27 12:28:42 -0700

    Changed Multiple

---
+++ object-construction-literal/script.js
@@ -0,0 +1,11 @@
+var car = {
+    speed: 0,
+    drive: function() {
+        this.speed = 60;
+    }
+};
+
+console.log("car speed:", car.speed);
+console.log("driving...")
+car.drive();
+console.log("car speed:", car.speed);
