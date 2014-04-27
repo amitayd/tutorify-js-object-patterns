c2ea7eea3bbb17d6245f5ccfe7710cb535c54758
Amitay Dobo <amitayd@gmail.com>
2014-04-27 15:00:59 -0700

    Changed Multiple

---
+++ prototypes-simple/script.js
@@ -0,0 +1,22 @@
+function Vehicle() {
+    this.drive = function () {
+        this.speed = this.maxSpeed;
+    }
+}
+
+function Car(maxSpeed) {
+    this.speed = 0;
+    this.maxSpeed = maxSpeed;
+}
+
+Car.prototype = new Vehicle();
+
+var bmw = new Car(160);
+console.log('bmw speed:', bmw.speed);
+bmw.drive();
+console.log('bmw speed (after drive):', bmw.speed);
+
+var susita = new Car(80);
+console.log('susita speed:', susita.speed);
+susita.drive();
+console.log('susita speed (after drive):', susita.speed);
