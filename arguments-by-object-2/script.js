ab6e51e96bb3ff0f6107adbc84915a3df4e78c08
Amitay Dobo <amitayd@gmail.com>
2014-04-26 17:31:14 -0700

    Changed Multiple

---
+++ arguments-by-object-2/script.js
@@ -0,0 +1,15 @@
+function argumentsPowSum(exponent) {
+    var sum = 0,
+        i = 1;
+
+    for (; i < arguments.length; i++) {
+        var power = Math.pow(arguments[i], exponent);
+        sum = sum + power;
+    }
+
+    return sum;
+}
+
+console.log(argumentsPowSum(2,4));
+//argumentsSum(1, 2, 3);
+//argumentsSum(2, 3);
